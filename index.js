d01f83ea464f5beda2a5ca8c0ef8529a3fcf5e75
linsk1998 <h@linsk.us>
2022-04-06 23:34:22 +0800

    new postcss API

--- index.js
+++ index.js
@@ -1,63 +1,43 @@
-var postcss = require('postcss');
-
-module.exports = postcss.plugin('postcss-opacity', function(opts) {
+module.exports = function(opts) {
   opts = opts || {};
-  opts.legacy = opts.legacy || false;
+  var legacy = opts.legacy || false;
+  var preserve = opts.preserve;
+  var moz = opts.moz;
+  var khtml = opts.khtml;
+  var ie8 = opts.ie8;
 
-  var PROP = 'opacity';
+  return {
+    postcssPlugin: 'postcss-opacity',
+    Declaration: {
+      opacity: decl => {
+        var subOne = decl.value;
+        var subHundred = Math.floor(subOne * 100);
 
-  return function(css) {
-
-    css.walkRules(function(rule) {
-      // Search through props, ignore current and insert whats missing
-
-      var propsToTestInsert = (function(def) {
-        return def.concat((opts.legacy) ? [{ prop: 'filter', order: 1 }, { prop: '-moz-opacity', order: 2 }, { prop: '-khtml-opacity', order: 3 }] : []);
-      })([{ prop: 'opacity', order: 4 }, { prop: '-ms-filter', order: 0 }]);
-
-      var propsToInsert = [];
-
-      propsToTestInsert.sort(function(a, b) {
-        return a.order > b.order;
-      }).forEach(function(v) {
-        // find if prop based on legacy is found
-        var isPropAlreadyPresent = false;
-        rule.walkDecls(v.prop, function() {
-          isPropAlreadyPresent = true;
-        });
-
-        if (!isPropAlreadyPresent) {
-          propsToInsert.push(v.prop);
+        if (ie8 !== false) {
+          if (!decl.parent.some(decl => decl.prop === '-ms-filter')) {
+            decl.cloneBefore({ prop: "-ms-filter", value: '"progid:DXImageTransform.Microsoft.Alpha(Opacity=' + subHundred + ')"' });
+          }
         }
-      });
-
-      propsToInsert.forEach(function(addProp) {
-        rule.walkDecls(PROP, function(decl) {
-          var value = void 0,
-            subOne = decl.value,
-            subHundred = Math.floor(subOne * 100);
-
-          switch (addProp) {
-            case 'opacity':
-            case '-moz-opacity':
-            case '-khtml-opacity':
-              value = subOne;
-              break;
-            case 'filter':
-              value = 'alpha(opacity=' + subHundred + ')';
-              break;
-            case '-ms-filter':
-              value = '"progid:DXImageTransform.Microsoft.Alpha(Opacity=' + subHundred + ')"';
-              break;
+        if (legacy) {
+          if (!decl.parent.some(decl => decl.prop === 'filter')) {
+            decl.cloneBefore({ prop: "filter", value: 'alpha(opacity=' + subHundred + ')' });
           }
-
-          if (value) {
-            // adds new property only if it's not present yet and we actually found a prop
-            rule.insertBefore(decl, { prop: addProp, value: value });
+        }
+        if (moz || (legacy && moz !== false)) {
+          if (!decl.parent.some(decl => decl.prop === '-moz-opacity')) {
+            decl.cloneBefore({ prop: "-moz-opacity", value: subOne });
           }
-        });
-      });
-    });
-
+        }
+        if (khtml || (legacy && khtml !== false)) {
+          if (!decl.parent.some(decl => decl.prop === '-khtml-opacity')) {
+            decl.cloneBefore({ prop: "-khtml-opacity", value: subOne });
+          }
+        }
+        if (preserve === false) {
+          decl.remove();
+        }
+      }
+    }
   };
-});
+};
+module.exports.postcss = true;
